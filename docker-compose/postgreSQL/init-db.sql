29904ebc45f1056ac04b1995b4f312b6584fcbc0
Tolo Castañerc Chacón <tomca467@gmail.com>
2022-03-23 09:30:57 +0100

    Modify init-db and add dependencies for infrastructure and application modules.

--- docker-compose/postgreSQL/init-db.sql
+++ docker-compose/postgreSQL/init-db.sql
@@ -12,14 +12,11 @@ SET row_security = off;
 DROP DATABASE IF EXISTS exercicedb;
 CREATE DATABASE exercicedb WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.UTF-8';
 
-CREATE SCHEMA MYSCHEMA;
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO "user";
+GRANT ALL ON SCHEMA public TO PUBLIC;
 
-REVOKE ALL ON SCHEMA MYSCHEMA FROM PUBLIC;
-GRANT ALL ON SCHEMA MYSCHEMA TO "user";
-GRANT ALL ON SCHEMA MYSCHEMA TO PUBLIC;
-
-
-CREATE TABLE IF NOT EXISTS MYSCHEMA.T_RATES (
+CREATE TABLE IF NOT EXISTS public.T_RATES (
     ID SERIAL PRIMARY KEY NOT NULL,
     BRAND_ID INT NOT NULL,
     PRODUCT_ID INT NOT NULL,
@@ -29,31 +26,31 @@ CREATE TABLE IF NOT EXISTS MYSCHEMA.T_RATES (
     CURRENCY_CODE VARCHAR(3) NOT NULL
 );
 
-CREATE INDEX rates_brand_id_index ON MYSCHEMA.T_RATES USING btree (brand_id);
-CREATE INDEX rates_product_id_index ON MYSCHEMA.T_RATES USING btree (product_id);
-CREATE INDEX rates_currency_code_index ON MYSCHEMA.T_RATES USING btree (currency_code);
+CREATE INDEX rates_brand_id_index ON public.T_RATES USING btree (brand_id);
+CREATE INDEX rates_product_id_index ON public.T_RATES USING btree (product_id);
+CREATE INDEX rates_currency_code_index ON public.T_RATES USING btree (currency_code);
 
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 1, 1, '2022-01-01', '2022-05-31', 1550, 'EUR');
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 1, 1, '2022-06-01', '2022-12-31', 1850, 'USD');
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 2, 1, '2022-01-01', '2022-05-31', 2050, 'EUR');
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 2, 1, '2022-06-01', '2022-12-31', 2250, 'USD');
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 1, 2, '2022-01-01', '2022-05-31', 2550, 'EUR');
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 1, 2, '2022-06-01', '2022-12-31', 2850, 'USD');
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 2, 2, '2022-01-01', '2022-05-31', 3050, 'EUR');
-INSERT INTO MYSCHEMA.t_rates
+INSERT INTO public.t_rates
 (brand_id, product_id, start_date, end_date, price, currency_code)
 VALUES( 2, 2, '2022-06-01', '2022-12-31', 3250, 'USD');
