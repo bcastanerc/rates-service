d1908408b6501c28300e140ce2ec144d42e26522
Tolo Castañerc Chacón <tomca467@gmail.com>
2022-04-01 09:25:48 +0200

    Created tests for Controller.

--- rates-service-app-api-rest/src/main/java/io/virtualcave/rates/api/rest/controllers/RateController.java
+++ rates-service-app-api-rest/src/main/java/io/virtualcave/rates/api/rest/controllers/RateController.java
@@ -6,16 +6,19 @@ import io.virtualcave.api.rest.dtos.v1.RateRequestDto;
 import io.virtualcave.rates.api.rest.mappers.RateDtoMapper;
 import io.virtualcave.rates.api.rest.mappers.RateRequestDtoMapper;
 import io.virtualcave.rates.application.commands.AddRateCommandHandler;
-import io.virtualcave.rates.application.commands.DeleteRateCommandHandler;
+import io.virtualcave.rates.application.commands.DeleteRateByIdCommandHandler;
+import io.virtualcave.rates.application.commands.UpdateRateAmountByIdCommandHandler;
+import io.virtualcave.rates.application.commands.UpdateRateAmountCommand;
 import io.virtualcave.rates.application.queries.FindRateByIdQuery;
 import io.virtualcave.rates.application.queries.FindRateByIdQueryHandler;
+import io.virtualcave.rates.application.queries.FindRatesByProductBrandIdQuery;
+import io.virtualcave.rates.application.queries.FindRatesByProductBrandIdQueryHandler;
+import java.net.URI;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
@@ -24,20 +27,18 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*",
-    methods = {
-        RequestMethod.GET,
-        RequestMethod.DELETE,
-        RequestMethod.POST,
-        RequestMethod.PUT
-    })
 public class RateController implements RatesApi {
 
     private final AddRateCommandHandler addRateCommandHandler;
 
-    private final DeleteRateCommandHandler deleteRateCommandHandler;
+    private final DeleteRateByIdCommandHandler deleteRateByIdCommandHandler;
+
+    private final UpdateRateAmountByIdCommandHandler updateRateAmountByIdCommandHandler;
 
     private final FindRateByIdQueryHandler findRateByIdQueryHandler;
+
+    private final FindRatesByProductBrandIdQueryHandler findRatesByProductBrandIdQueryHandler;
+
 
     private final RateDtoMapper rateDtoMapper;
 
@@ -48,7 +49,7 @@ public class RateController implements RatesApi {
         return rateRequestDto.map(rateRequestDtoMapper::asRateRequest)
             .flatMap(addRateCommandHandler::executeAndReturn)
             .map(rateDtoMapper::asRateDto)
-            .map(ResponseEntity::ok);
+            .map(rateDto -> ResponseEntity.created(URI.create(String.format("/v1/rate/%s", rateDto.getId()))).body(rateDto));
     }
 
     @Override
@@ -61,20 +62,25 @@ public class RateController implements RatesApi {
 
     @Override
     public Mono<ResponseEntity<Void>> deleteById(String id, ServerWebExchange exchange) {
-        return deleteRateCommandHandler.executeAndReturn(id)
+        return deleteRateByIdCommandHandler.executeAndReturn(id)
             .thenReturn(new ResponseEntity<Void>(HttpStatus.NO_CONTENT))
             .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @Override
-    public Mono<ResponseEntity<RateDto>> updateRateById(String id, String price, ServerWebExchange exchange) {
-        return Mono.empty();
+    public Mono<ResponseEntity<RateDto>> updateRateById(String id, Integer price, ServerWebExchange exchange) {
+       return updateRateAmountByIdCommandHandler.executeAndReturn(UpdateRateAmountCommand.builder().id(id).price(price).build())
+           .map(rateDtoMapper::asRateDto)
+           .map(ResponseEntity::ok)
+           .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @Override
     public Mono<ResponseEntity<Flux<RateDto>>> findRateByProductAndBrand(LocalDate startDate, String brandId, String productId,
         ServerWebExchange exchange) {
-        return Mono.empty();
+        return Mono.just(ResponseEntity.ok(findRatesByProductBrandIdQueryHandler.execute(FindRatesByProductBrandIdQuery.builder().productId(productId).brandId(brandId).startDate(startDate).build())
+            .map(rateDtoMapper::asRateDto)))
+            .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
 }
--- rates-service-app-api-rest/src/test/java/io/virtualcave/rates/api/rest/RateApiController.java
+++ rates-service-app-api-rest/src/test/java/io/virtualcave/rates/api/rest/RateApiController.java
@@ -6,10 +6,17 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import io.virtualcave.api.rest.dtos.v1.RateRequestDto;
+import io.virtualcave.rates.api.rest.controllers.RateController;
 import io.virtualcave.rates.api.rest.mappers.RateDtoMapperImpl;
-import io.virtualcave.rates.api.rest.mappers.RateRequestDtoMapper;
+import io.virtualcave.rates.api.rest.mappers.RateRequestDtoMapperImpl;
 import io.virtualcave.rates.application.commands.AddRateCommandHandler;
+import io.virtualcave.rates.application.commands.DeleteRateByIdCommandHandler;
+import io.virtualcave.rates.application.commands.UpdateRateAmountByIdCommandHandler;
+import io.virtualcave.rates.application.commands.UpdateRateAmountCommand;
+import io.virtualcave.rates.application.queries.FindRateByIdQuery;
+import io.virtualcave.rates.application.queries.FindRateByIdQueryHandler;
 import io.virtualcave.rates.domain.DomainObjectMother;
+import io.virtualcave.rates.model.Rate;
 import io.virtualcave.rates.model.RateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,17 +28,26 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 @WebFluxTest
-@ContextConfiguration(classes = {RateApiController.class})
+@ContextConfiguration(classes = {RateController.class, RateExceptionHandler.class})
 public class RateApiController {
 
   @MockBean
   private AddRateCommandHandler addRateCommandHandler;
 
+  @MockBean
+  private  UpdateRateAmountByIdCommandHandler updateRateAmountByIdCommandHandler;
+
+  @MockBean
+  private DeleteRateByIdCommandHandler deleteRateByIdCommandHandler;
+
+  @MockBean
+  private  FindRateByIdQueryHandler findRateByIdQueryHandler;
+
   @SpyBean
   private RateDtoMapperImpl rateDtoMapper;
 
   @SpyBean
-  private RateRequestDtoMapper rateRequestDtoMapper;
+  private RateRequestDtoMapperImpl rateRequestDtoMapper;
 
   @Autowired
   private WebTestClient testClient;
@@ -50,13 +66,60 @@ public class RateApiController {
     .exchange()
     .expectStatus().isCreated()
     .expectHeader().contentType(APPLICATION_JSON)
-    .expectBody(RateRequestDto.class)
+    .expectBody(Rate.class)
     .consumeWith(response -> {
-      RateRequestDto responseBody = response.getResponseBody();
+      Rate responseBody = response.getResponseBody();
       assertThat(responseBody).isNotNull();
-      assertThat(responseBody.getTotalAmount()).isEqualTo(1704);
-      assertThat(responseBody.getBrandId()).isEqualTo("2");
-      assertThat(responseBody.getProductId()).isEqualTo("3");
+      assertThat(responseBody.getAmount().getValue()).isEqualTo(rateRequest.getTotalAmount());
+      assertThat(responseBody.getBrandId()).isEqualTo(rateRequest.getBrandId());
+      assertThat(responseBody.getProductId()).isEqualTo(rateRequest.getProductId());
     });
+  }
+
+  @Test
+  void findRateById_ShouldReturnRateWithMachingId_WhenSuccess() {
+
+    given(findRateByIdQueryHandler.execute(FindRateByIdQuery.builder().rateId("1").build()))
+        .willReturn(Mono.just(DomainObjectMother.rate()));
+
+    testClient.get().uri("/v1/rates/1")
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isOk()
+        .expectHeader().contentType(APPLICATION_JSON)
+        .expectBody(Rate.class)
+        .consumeWith(response -> {
+          Rate responseBody = response.getResponseBody();
+          assertThat(responseBody).isNotNull();
+          assertThat(responseBody.getId()).isEqualTo(1);
+          assertThat(responseBody.getAmount().getValue()).isEqualTo(1704);
+          assertThat(responseBody.getBrandId()).isEqualTo(2);
+          assertThat(responseBody.getProductId()).isEqualTo(3);
+        });
+  }
+
+  @Test
+  void updateById_ShouldReturnRateAndUpdateItsAmmount_WhenSuccess() {
+
+    Rate rateResponse = DomainObjectMother.rate();
+    rateResponse.getAmount().setValue(1111);
+
+    given(updateRateAmountByIdCommandHandler.executeAndReturn(UpdateRateAmountCommand.builder().id("1").price(1111).build()))
+        .willReturn(Mono.just(rateResponse));
+
+    testClient.patch().uri("/v1/rates/1")
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isOk()
+        .expectHeader().contentType(APPLICATION_JSON)
+        .expectBody(Rate.class)
+        .consumeWith(response -> {
+          Rate responseBody = response.getResponseBody();
+          assertThat(responseBody).isNotNull();
+          assertThat(responseBody.getId()).isEqualTo(rateResponse.getId());
+          assertThat(responseBody.getAmount().getValue()).isEqualTo(rateResponse.getAmount().getValue());
+          assertThat(responseBody.getBrandId()).isEqualTo(rateResponse.getBrandId());
+          assertThat(responseBody.getProductId()).isEqualTo(rateResponse.getProductId());
+        });
   }
 }
